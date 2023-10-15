896ccb9f19bb9444bca53457d7576848bd68c79a
Mohamad Chebli <mohamed.chebli@lau.edu>
2023-10-15 22:22:38 +0300

    Initial commit

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FinishScreen({ points, maxPossiblePoints, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  return (
+    <>
+      <p className="result">
+        You Scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+          </p>
+          <button className="btn btn-ui" onClick={()=>dispatch({type:"restart"})}>Restart Quiz</button>
+    </>
+  );
+}
+
+export default FinishScreen;
